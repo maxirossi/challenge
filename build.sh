a1f8dc6a737378cfeec839f9e877910c534f9519
Maximiliano Rossi <rossi.maxi@gmail.com>
2025-05-25 17:55:41 -0400

    comment prune

--- build.sh
+++ build.sh
@@ -17,10 +17,10 @@ fi
 cd ..
 
 # (Optional) Comment if you don't want clear volumes
-echo "🧹 Cleaning Docker environment..."
-docker-compose down --volumes --remove-orphans
-docker system prune -af
-docker volume prune -f
+#echo "🧹 Cleaning Docker environment..."
+#docker-compose down --volumes --remove-orphans
+#docker system prune -af
+#docker volume prune -f
 
 echo "🚀 Building and starting containers..."
 docker-compose up --build
